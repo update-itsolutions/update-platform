811b8328af9a7ce09425ae860a9f9888655b425f
Leandro Aniceto <lea.aniceto.90@gmail.com>
2026-08-01 18:18:40 -0300

    Fix render requirements

--- app/database.py
+++ app/database.py
@@ -3,7 +3,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgress:iV2ur5GaLe2iBPqZveJzubtzfQLnYj5k@dpg-d9n5sim1egvs73ff4tvg-a.oregon-postgres.render.com/update_platform"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
